7b25fc3318053f9a8b5443b7b87d0b097cb22c6b
Sean McGary <sean.mcgary@gmail.com>
2012-09-01 13:24:14 -0700

    fixing test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,12 @@
 var sys = require('sys'),
     fs = require('fs'),
-    im = require('../index.js');
+    im = require('../index.js'),
+	path = require('path');
 
-var path = 'sample-images/jpeg5.jpg';
-var imdata = fs.readFileSync(path, 'binary');
+var image_path = path.normalize(__dirname + '/img.jpg');
+var imdata = fs.readFileSync(image_path, 'binary');
 
-im.identify(path, function(err, features){
+im.identify(image_path, function(err, features){
   if (err) return sys.error(err.stack || err);
   sys.puts('features: '+sys.inspect(features));
 })
@@ -15,7 +16,7 @@ im.identify({data:imdata}, function(err, features){
   sys.puts('features: '+sys.inspect(features));
 })
 
-im.readMetadata(path, function(err, metadata){
+im.readMetadata(image_path, function(err, metadata){
   if (err) return sys.error(err.stack || err);
   sys.puts('metadata: '+sys.inspect(metadata));
 })
@@ -27,13 +28,13 @@ im.readMetadata({data:imdata}, function(err, metadata){
 
 var timeStarted = new Date;
 im.resize({
-  srcPath: path,
-  dstPath: path+'.resized.jpg',
+  srcPath: image_path,
+  dstPath: image_path+'.resized.jpg',
   width: 256
 }, function(err, stdout, stderr){
   if (err) return sys.error(err.stack || err);
   sys.puts('real time taken for convert: '+((new Date)-timeStarted)+' ms')
-  im.identify(['-format', '%b', path+'.resized.jpg'], function(err, r){
+  im.identify(['-format', '%b', image_path+'.resized.jpg'], function(err, r){
     if (err) throw err;
     sys.puts('size: '+r.substr(0,r.length-2)+' Bytes');
   })
@@ -46,6 +47,6 @@ im.resize({
 }, function(err, stdout, stderr){
   if (err) return sys.error(err.stack || err);
   sys.puts('real time taken for convert (with buffers): '+((new Date)-timeStarted)+' ms');
-  fs.writeFileSync(path+'.resized-io.jpg', stdout, 'binary');
+  fs.writeFileSync(image_path+'.resized-io.jpg', stdout, 'binary');
   sys.puts('size: '+stdout.length+' Bytes');
 })
